60ea4e6a0c1c6819b225df911d7c76d8e770bbf5
elaforge98 <51786616+elaforge98@users.noreply.github.com>
2021-02-08 18:41:18 -0500

    start gui

---
+++ src/gui/App.java
@@ -0,0 +1,15 @@
+package gui;
+
+public class App {
+
+    public static void main(String[] args) {
+
+		LoginFrame fenetre = new LoginFrame();
+
+		//environnement.addPropertyChangeListener(fenetre);
+		//environnement.execute();
+
+
+	}
+    
+}
---
+++ src/gui/LoginFrame.java
@@ -0,0 +1,26 @@
+package gui; 
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+
+public class LoginFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private static final String TITRE_FENETRE = "Webflix";
+		private static final Dimension DIMENSION = new Dimension(700, 700);
+
+
+	public LoginFrame() {
+		
+		setVisible(true);
+		setTitle(TITRE_FENETRE);
+		// Mettre la fen�tre au centre de l'�cran
+		setLocationRelativeTo(null);
+		// Emp�cher la redimension de la fen�tre
+		setResizable(false);
+		setSize(DIMENSION);
+
+	}
+
+}
